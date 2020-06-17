4e6e242a0894ae0c974353a98cc5f4cafbff73c7
MOHD AFSAN AHMAD <mafsan78692@gmail.com>
2020-06-17 08:13:28 +0530

    cancel buuton and all other things

--- meet.sql
+++ meet.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.6deb5
+-- version 4.9.5deb2
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Feb 24, 2020 at 02:14 PM
--- Server version: 8.0.19-0ubuntu0.19.10.3
--- PHP Version: 7.2.19-0ubuntu0.19.04.2
+-- Generation Time: Jun 17, 2020 at 08:11 AM
+-- Server version: 8.0.20-0ubuntu0.20.04.1
+-- PHP Version: 7.4.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -35,63 +37,64 @@ CREATE TABLE `doctor` (
   `specialization` varchar(255) NOT NULL,
   `fee` varchar(100) NOT NULL,
   `address` varchar(250) NOT NULL,
-  `current_number` int NOT NULL DEFAULT '0'
-);
+  `current_number` int NOT NULL DEFAULT '0',
+  `loginid` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `doctor`
 --
 
-INSERT INTO `doctor` (`doctorid`, `doctorname`, `gender`, `location`, `picid`, `specialization`, `fee`, `address`, `current_number`) VALUES
-(1, 'Dr. Cherian Mamen', 'M', 'kochi', '1.jpg', 'General Physicians', '400', '', 0),
-(2, 'Dr. Rajkumar', 'M', 'kochi', '2.jpg', 'Dentist', '600', '', 0),
-(3, 'Dr. Savita Bhat', 'F', 'kochi', '3.jpg', 'Cardiologists', '430', '', 0),
-(4, 'Dr. Siju C. Cheeran', 'M', 'kochi', '4.jpg', 'Dentist', '550', '', 0),
-(5, 'Dr. Rose V. Pulikkal ', 'F', 'kochi', '5.jpg', 'General Physicians', '800', '', 0),
-(6, 'Dr. Deepa Paulose', 'F', 'kochi', '6.jpg', 'Cardiologists', '464', '', 0),
-(7, 'Dr. Aditya K. B.', 'M', 'Palakkad', '7.jpg', 'Dentist', '4000', '', 0),
-(8, 'Dr. Revati S. Ramesh', 'M', 'Palakkad', '8.jpg', 'Psychiatrist', '8000', '', 0),
-(9, 'Dr. Leena Pillai', 'F', 'Palakkad', '9.jpg', 'Cardiologists', '650', '', 0),
-(10, 'Dr.  Puthuran Varghese', 'F', 'Palakkad', '10.jpg', 'General Physicians', '500', '', 0),
-(11, 'Dr. Apoorva S.', 'M', 'Palakkad', '11.jpg', 'Psychiatrist', '750', '', 0),
-(12, 'Dr. Narayanan Kutty', 'M', 'Palakkad', '12.jpg', 'Dentist', '800', '', 0),
-(13, 'Dr.  Kocha Varma', 'M', 'Trivandrum', '13.jpg', 'General Physicians', '665', '', 0),
-(14, 'Dr. Vijay Kumar M', 'F', 'Trivandrum', '14.jpg', 'Nephrologist', '800', '', 0),
-(15, ' Dr. Anil S.R', 'F', 'Trivandrum', '15.jpg', 'Orthopedist', '650', '', 0),
-(16, 'Dr. Apurv Shukla', 'M', 'Trivandrum', '16.jpg', 'General Physicians', '750', '', 0),
-(17, 'Dr. K. U Natarajan', 'F', 'Trivandrum', '17.jpg', 'Nephrologist', '250', '', 0),
-(18, 'Dr. C. V. Droupathy', 'M', 'Trivandrum', '18.jpg', 'Cardiologists', '850', '', 0),
-(19, 'Dr. Ravi Kuruvilla', 'F', 'Thrissur', '19.jpg', 'Dentist', '1100', '', 0),
-(20, 'Dr. Abhishek Mathew', 'F', 'Thrissur', '20.jpg', 'Orthopedist', '865', '', 0),
-(21, 'Dr. Alexander T. A.', 'M', 'Thrissur', '21.jpg', 'Psychiatrist', '900', '', 0),
-(22, 'Dr. Jacob Joseph', 'M', 'Thrissur', '22.jpg', 'Surgeon', '240', '', 0),
-(23, 'Dr. Dr. K. K Haridas', 'F', 'Kasargod', '23.jpg', 'General Physicians', '550', '', 0),
-(24, 'Dr. Anant Vats', 'F', 'Kasargod', '24.jpg', 'Orthopedist', '650', '', 0),
-(25, 'Dr.  Tixon Thomas', 'M', 'Kasargod', '25.jpg', 'Cardiologists', '780', '', 0),
-(26, 'Dr. Suma Job', 'F', 'Kasargod', '26.jpg', 'Nephrologist', '900', '', 0),
-(27, 'Dr. Akhil Kashyap', 'M', 'Kasargod', '27.jpg', 'Dentist', '650', '', 0),
-(28, 'Dr.  Sri Kanth Mothy', 'M', 'Wayand', '28.jpg', 'Surgeon', '647', '', 0),
-(29, 'Dr. Cherian Mamen', 'F', 'Wayand', '29.jpg', 'General Physicians', '1400', '', 0),
-(30, 'Dr. Antony Clement', 'F', 'Wayand', '30.jpg', 'Dentist', '900', '', 0),
-(31, 'Dr. Alexander K. S.', 'M', 'Wayand', '31.jpg', 'Psychiatrist', '450', '', 0),
-(32, 'Dr. Shrish George', 'F', 'Wayand', '32.jpg', 'Surgeon', '690', '', 0),
-(33, 'Dr. Warrier K. K. R.', 'F', 'Kozhikode', '33.jpg', 'Cardiologists', '750', '', 0),
-(34, 'Dr. Sreelatha V. K.', 'M', 'Kozhikode', '34.jpg', 'Orthopedist', '250', '', 0),
-(35, 'Dr. Mohit Suri', 'F', 'Kozhikode', '35.jpg', 'Surgeon', '365', '', 0),
-(36, 'Dr. Neelkamal', 'F', 'Kozhikode', '36.jpg', 'General Physicians', '145', '', 0),
-(37, 'Dr. Aparna K. A.', 'M', 'Kozhikode', '37.jpg', 'Dentist', '689', '', 0),
-(38, 'Dr. Radhalakshmy A.', 'M', 'Kozhikode', '38.jpg', 'Cardiologists', '478', '', 0),
-(39, 'Dr. Shaheer T. B.', 'M', 'Kottayam', '39.jpg', 'Surgeon', '500', '', 0),
-(40, 'Dr. Shanta Kurup', 'F', 'Kottayam', '40.jpg', 'Psychiatrist', '690', '', 0),
-(41, 'Dr. Hritick Ambuj', 'F', 'Kottayam', '41.jpg', 'Orthopedist', '480', '', 0),
-(42, 'Dr. Amiy K. M.', 'M', 'Kottayam', '42.jpg', 'Dentist', '980', '', 0),
-(43, 'Dr. Varma Kocha', 'M', 'Kottayam', '43.jpg', 'Surgeon', '650', '', 0),
-(44, 'Dr. Spravin Dass', 'F', 'Kottayam', '44.jpg', 'Dr. Spravin Dass', '750', '', 0),
-(45, 'Dr. Alok Thakur', 'M', 'Kannur', '45.jpg', 'Surgeon', '450', '', 0),
-(46, 'Dr. Gerogie Thomas', 'M', 'Kannur', '46.jpg', 'Dentist', '682', '', 0),
-(47, 'Dr.  Sony BabY', 'F', 'Kannur', '47.jpg', 'Nephrologist', '1400', '', 0),
-(48, 'Dr. Apoorva Ranjan', 'F', 'Kannur', '48.jpg', 'Orthopedist', '1500', '', 0),
-(49, 'Dr. Aman Hisariya', 'M', 'Kannur', '49.jpg', 'General Physicians', '1600', '', 0);
+INSERT INTO `doctor` (`doctorid`, `doctorname`, `gender`, `location`, `picid`, `specialization`, `fee`, `address`, `current_number`, `loginid`) VALUES
+(1, 'Dr. Cherian Mamen', 'M', 'kochi', '1.jpg', 'General Physicians', '400', '', 0, 0),
+(2, 'Dr. Rajkumar', 'M', 'kochi', '2.jpg', 'Dentist', '600', '', 0, 3),
+(3, 'Dr. Savita Bhat', 'F', 'kochi', '3.jpg', 'Cardiologists', '430', '', 2, 0),
+(4, 'Dr. Siju C. Cheeran', 'M', 'kochi', '4.jpg', 'Dentist', '550', '', 0, 4),
+(5, 'Dr. Rose V. Pulikkal ', 'F', 'kochi', '5.jpg', 'General Physicians', '800', '', 0, 0),
+(6, 'Dr. Deepa Paulose', 'F', 'kochi', '6.jpg', 'Cardiologists', '464', '', 0, 0),
+(7, 'Dr. Aditya K. B.', 'M', 'Palakkad', '7.jpg', 'Dentist', '400', '', 0, 0),
+(8, 'Dr. Revati S. Ramesh', 'M', 'Palakkad', '8.jpg', 'Psychiatrist', '800', '', 0, 0),
+(9, 'Dr. Leena Pillai', 'F', 'Palakkad', '9.jpg', 'Cardiologists', '650', '', 0, 0),
+(10, 'Dr.  Puthuran Varghese', 'F', 'Palakkad', '10.jpg', 'General Physicians', '500', '', 0, 0),
+(11, 'Dr. Apoorva S.', 'M', 'Palakkad', '11.jpg', 'Psychiatrist', '750', '', 0, 0),
+(12, 'Dr. Narayanan Kutty', 'M', 'Palakkad', '12.jpg', 'Dentist', '800', '', 0, 0),
+(13, 'Dr.  Kocha Varma', 'M', 'Trivandrum', '13.jpg', 'General Physicians', '665', '', 0, 0),
+(14, 'Dr. Vijay Kumar M', 'F', 'Trivandrum', '14.jpg', 'Nephrologist', '800', '', 0, 0),
+(15, ' Dr. Anil S.R', 'F', 'Trivandrum', '15.jpg', 'Orthopedist', '650', '', 0, 0),
+(16, 'Dr. Apurv Shukla', 'M', 'Trivandrum', '16.jpg', 'General Physicians', '750', '', 0, 0),
+(17, 'Dr. K. U Natarajan', 'F', 'Trivandrum', '17.jpg', 'Nephrologist', '250', '', 0, 0),
+(18, 'Dr. C. V. Droupathy', 'M', 'Trivandrum', '18.jpg', 'Cardiologists', '850', '', 0, 0),
+(19, 'Dr. Ravi Kuruvilla', 'F', 'Thrissur', '19.jpg', 'Dentist', '500', '', 0, 0),
+(20, 'Dr. Abhishek Mathew', 'F', 'Thrissur', '20.jpg', 'Orthopedist', '865', '', 0, 0),
+(21, 'Dr. Alexander T. A.', 'M', 'Thrissur', '21.jpg', 'Psychiatrist', '900', '', 0, 0),
+(22, 'Dr. Jacob Joseph', 'M', 'Thrissur', '22.jpg', 'Surgeon', '240', '', 0, 0),
+(23, 'Dr. Dr. K. K Haridas', 'F', 'Kasargod', '23.jpg', 'General Physicians', '550', '', 0, 0),
+(24, 'Dr. Anant Vats', 'F', 'Kasargod', '24.jpg', 'Orthopedist', '650', '', 0, 0),
+(25, 'Dr.  Tixon Thomas', 'M', 'Kasargod', '25.jpg', 'Cardiologists', '780', '', 0, 0),
+(26, 'Dr. Suma Job', 'F', 'Kasargod', '26.jpg', 'Nephrologist', '900', '', 0, 0),
+(27, 'Dr. Akhil Kashyap', 'M', 'Kasargod', '27.jpg', 'Dentist', '650', '', 0, 0),
+(28, 'Dr.  Sri Kanth Mothy', 'M', 'Wayand', '28.jpg', 'Surgeon', '647', '', 0, 0),
+(29, 'Dr. Cherian Mamen', 'F', 'Wayand', '29.jpg', 'General Physicians', '340', '', 0, 0),
+(30, 'Dr. Antony Clement', 'F', 'Wayand', '30.jpg', 'Dentist', '900', '', 0, 0),
+(31, 'Dr. Alexander K. S.', 'M', 'Wayand', '31.jpg', 'Psychiatrist', '450', '', 0, 0),
+(32, 'Dr. Shrish George', 'F', 'Wayand', '32.jpg', 'Surgeon', '690', '', 0, 0),
+(33, 'Dr. Warrier K. K. R.', 'F', 'Kozhikode', '33.jpg', 'Cardiologists', '750', '', 0, 0),
+(34, 'Dr. Sreelatha V. K.', 'M', 'Kozhikode', '34.jpg', 'Orthopedist', '250', '', 0, 0),
+(35, 'Dr. Mohit Suri', 'F', 'Kozhikode', '35.jpg', 'Surgeon', '365', '', 0, 0),
+(36, 'Dr. Neelkamal', 'F', 'Kozhikode', '36.jpg', 'General Physicians', '145', '', 0, 0),
+(37, 'Dr. Aparna K. A.', 'M', 'Kozhikode', '37.jpg', 'Dentist', '689', '', 0, 0),
+(38, 'Dr. Radhalakshmy A.', 'M', 'Kozhikode', '38.jpg', 'Cardiologists', '478', '', 0, 0),
+(39, 'Dr. Shaheer T. B.', 'M', 'Kottayam', '39.jpg', 'Surgeon', '500', '', 0, 0),
+(40, 'Dr. Shanta Kurup', 'F', 'Kottayam', '40.jpg', 'Psychiatrist', '690', '', 0, 0),
+(41, 'Dr. Hritick Ambuj', 'F', 'Kottayam', '41.jpg', 'Orthopedist', '480', '', 0, 0),
+(42, 'Dr. Amiy K. M.', 'M', 'Kottayam', '42.jpg', 'Dentist', '980', '', 0, 0),
+(43, 'Dr. Varma Kocha', 'M', 'Kottayam', '43.jpg', 'Surgeon', '650', '', 0, 0),
+(44, 'Dr. Spravin Dass', 'F', 'Kottayam', '44.jpg', 'Dr. Spravin Dass', '750', '', 0, 0),
+(45, 'Dr. Alok Thakur', 'M', 'Kannur', '45.jpg', 'Surgeon', '450', '', 0, 0),
+(46, 'Dr. Gerogie Thomas', 'M', 'Kannur', '46.jpg', 'Dentist', '682', '', 0, 0),
+(47, 'Dr.  Sony BabY', 'F', 'Kannur', '47.jpg', 'Nephrologist', '350', '', 0, 0),
+(48, 'Dr. Apoorva Ranjan', 'F', 'Kannur', '48.jpg', 'Orthopedist', '500', '', 0, 0),
+(49, 'Dr. Aman Hisariya', 'M', 'Kannur', '49.jpg', 'General Physicians', '600', '', 0, 0);
 
 -- --------------------------------------------------------
 
@@ -104,7 +107,15 @@ CREATE TABLE `doctor_check` (
   `doctorid` int NOT NULL,
   `patientid` int NOT NULL,
   `checked_or_canceled` int DEFAULT NULL
-);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `doctor_check`
+--
+
+INSERT INTO `doctor_check` (`id`, `doctorid`, `patientid`, `checked_or_canceled`) VALUES
+(4, 3, 4, 1),
+(5, 4, 5, 1);
 
 -- --------------------------------------------------------
 
@@ -117,7 +128,7 @@ CREATE TABLE `login` (
   `email` varchar(255) NOT NULL,
   `password` varchar(255) DEFAULT NULL,
   `flag` int NOT NULL
-);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `login`
@@ -125,7 +136,9 @@ CREATE TABLE `login` (
 
 INSERT INTO `login` (`id`, `email`, `password`, `flag`) VALUES
 (1, 'afsan@gmail.com', '$2a$10$eagHcEz3/EtTimXglvyc4OLHfBZWUoqrTSq6ONnPIZOVdHlFAMulO', 0),
-(2, 'afsan786@gmail.com', '$2a$10$Yg9lCjsNa1wbiasgsyCxKuRLD4qPu7P1xbcJvX9zB9.uTw6na.u2q', 0);
+(2, 'afsan786@gmail.com', '$2a$10$Yg9lCjsNa1wbiasgsyCxKuRLD4qPu7P1xbcJvX9zB9.uTw6na.u2q', 0),
+(3, 'rajkumar@gmail.com', '$2a$10$Yg9lCjsNa1wbiasgsyCxKuRLD4qPu7P1xbcJvX9zB9.uTw6na.u2q', 2),
+(4, 'siju@gmail.com', '$2a$10$Yg9lCjsNa1wbiasgsyCxKuRLD4qPu7P1xbcJvX9zB9.uTw6na.u2q', 2);
 
 -- --------------------------------------------------------
 
@@ -137,7 +150,7 @@ CREATE TABLE `max_limit` (
   `id` int NOT NULL,
   `doctorid` int NOT NULL,
   `max_number` int NOT NULL
-);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `max_limit`
@@ -202,31 +215,23 @@ INSERT INTO `max_limit` (`id`, `doctorid`, `max_number`) VALUES
 
 CREATE TABLE `patient` (
   `patientid` int NOT NULL,
-  `lastname` varchar(255) NOT NULL,
-  `firstname` varchar(255) DEFAULT NULL,
+  `lastname` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `firstname` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   `gender` varchar(20) NOT NULL,
   `age` int NOT NULL,
   `mstatus` varchar(250) NOT NULL,
   `address` varchar(1000) NOT NULL,
   `date` varchar(250) NOT NULL,
   `loginid` int NOT NULL
-);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `patient`
 --
 
 INSERT INTO `patient` (`patientid`, `lastname`, `firstname`, `gender`, `age`, `mstatus`, `address`, `date`, `loginid`) VALUES
-(1, 'dhgd', 'xyz', 'male', 23, 'Unmarried', 'hbrtj', '2019-11-10', 1),
-(2, 'rgwe', 'rewtgwe', 'male', 3245, 'Unmarried', 'rwegwe', '2019-11-17', 1),
-(3, 'ergrwe', 'ertgerw', 'male', 3465, 'Unmarried', 'gfhrth', '2019-11-21', 1),
-(4, 'gwe', 'fdgerg', 'male', 34, 'Unmarried', 'ergeh', '2019-11-24', 1),
-(5, 'wsegfwe', 'sdwsdeg', 'male', 345, 'Unmarried', 'dfbeber', '2019-11-05', 1),
-(6, 'dfhd', 'fsdbgdfh', 'male', 5643, 'Unmarried', 'herh', '2019-11-17', 1),
-(7, 'dfher', 'dfherh', 'male', 4564, 'Unmarried', 'deherhr', '2019-11-16', 1),
-(8, 'ameer', 'salman', 'male', 20, 'Unmarried', 'kochi', '1999-03-15', 1),
-(10, 's nair', 'megha', 'male', 20, 'Unmarried', 'asdnjkaDS', '2020-01-07', 1),
-(11, 'HHh', 'HSHFGJ', 'male', 45, 'Unmarried', 'kfuhvguiefg', '2020-01-06', 1);
+(4, 'hvh', 'xyz', 'male', 22, 'Unmarried', 'jyviyvyi', '2020-06-17', 1),
+(5, 'hvjgc', 'sdgsdg', 'male', 22, 'Unmarried', 'jhdvvd', '2020-06-17', 1);
 
 -- --------------------------------------------------------
 
@@ -241,7 +246,7 @@ CREATE TABLE `ptdoctor` (
   `loginid` int NOT NULL,
   `appointment_date` varchar(30) NOT NULL,
   `appointment_number` int NOT NULL
-);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Indexes for dumped tables
@@ -298,21 +303,37 @@ ALTER TABLE `ptdoctor`
 --
 ALTER TABLE `doctor`
   MODIFY `doctorid` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=50;
+
+--
+-- AUTO_INCREMENT for table `doctor_check`
+--
+ALTER TABLE `doctor_check`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
 --
 -- AUTO_INCREMENT for table `login`
 --
 ALTER TABLE `login`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
 --
 -- AUTO_INCREMENT for table `max_limit`
 --
 ALTER TABLE `max_limit`
   MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=50;
+
 --
 -- AUTO_INCREMENT for table `patient`
 --
 ALTER TABLE `patient`
-  MODIFY `patientid` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `patientid` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `ptdoctor`
+--
+ALTER TABLE `ptdoctor`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
 --
 -- Constraints for dumped tables
 --
@@ -337,6 +358,7 @@ ALTER TABLE `ptdoctor`
   ADD CONSTRAINT `patient_doctor_ibfk_1` FOREIGN KEY (`patientid`) REFERENCES `patient` (`patientid`),
   ADD CONSTRAINT `patient_doctor_ibfk_2` FOREIGN KEY (`doctorid`) REFERENCES `doctor` (`doctorid`),
   ADD CONSTRAINT `patient_doctor_ibfk_3` FOREIGN KEY (`loginid`) REFERENCES `login` (`id`);
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
